bf5e39131c388a764e8df14a3404d52d2c8586bd
元皓yh <hw@yuanhau.com>
2025-02-15 18:05:56 +0800

    Update create.php

--- create.php
+++ create.php
@@ -1,8 +1,14 @@
 <?php
 $actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $link = str_replace("https://scripts.yhw.tw/create.php?", "", $actual_link);
-if ($link.include("../") {
+if (strpos($link, "../") !== false) {
     die("error");
 }
+
 $myfile = fopen($link, "w") or die("Unable to open file!");
+
 ?>
+<form submit="create.php" method="POST">
+    <textarea/>
+    <input type="text" />
+</form>
