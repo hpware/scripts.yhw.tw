511329a92bb0b0dd51a5fa83a86ccccdfc98053e
元皓yh <hw@yuanhau.com>
2025-02-15 18:01:33 +0800

    Update create.php

--- create.php
+++ create.php
@@ -1,6 +1,8 @@
 <?php
 $actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$link = str_replace("https://scripts.yhw.tw/create.php", "", $actual_link);
-
-echo $link;
+$link = str_replace("https://scripts.yhw.tw/create.php?", "", $actual_link);
+if ($link.include("../") {
+    die("error");
+}
+$myfile = fopen($link, "w") or die("Unable to open file!");
 ?>
