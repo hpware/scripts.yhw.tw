3591762a7919c3e5afdfab8d1972b281fedd174e
元皓yh <hw@yuanhau.com>
2025-02-15 18:08:34 +0800

    Update create.php

--- create.php
+++ create.php
@@ -1,15 +1,22 @@
 <?php
 $actual_link = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $link = str_replace("https://scripts.yhw.tw/create.php?", "", $actual_link);
+
 if (strpos($link, "../") !== false) {
     die("error");
 }
 
-$myfile = fopen($link, "w");
-
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $content = $_POST['content'] ?? '';
+    $myfile = fopen($link, "w") or die("Unable to open file!");
+    fwrite($myfile, $content);
+    fclose($myfile);
+    echo "File written successfully";
+}
 ?>
-<form submit="create.php" method="POST">
-    <textarea></textarea>
-    <input type="text" />
+
+<form action="create.php" method="POST">
+    <textarea name="content"></textarea>
+    <input type="text" name="filename" />
     <input type="submit"/>
 </form>
