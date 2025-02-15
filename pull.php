8a457b5e36d255c194f92025ae10fad8bcec9011
元皓yh <hw@yuanhau.com>
2025-02-15 17:49:54 +0800

    Update pull.php

--- pull.php
+++ pull.php
@@ -1,4 +1,5 @@
 <?php
-    $output_status = shell_exec("git config --global --add safe.directory /var/www/scripts.yhw.tw && cd /var/www/scripts.yhw.tw/ && git pull");
+    $contents = file_get_contents("./scripts-server/gitpull.sh");
+    $output_status = shell_exec($contents);
     echo ("done? \n $output_status");
 ?>
